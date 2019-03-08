b9a9e938414629388a48053526433dc8da592b56
Jia Hao <pohjiahao@hotmail.com>
2019-03-08 20:46:57 +0800

    Counting Sort w/ Negatives

--- Sorting/CountingSort.java
+++ Sorting/CountingSort.java
@@ -3,15 +3,16 @@ import java.util.stream.IntStream;
 class CountingSort {
 
     static void countingSort(OrderEnum.Order order, int[] inputArr) {
-        //Assume range of integers are from 0-100
+
         int max = IntStream.of(inputArr).max().getAsInt();
-        int countArr[] = new int[max + 1];
+        int min = IntStream.of(inputArr).min().getAsInt();
+        int countArr[] = new int[max - min + 1];
         int outputArr[] = new int[inputArr.length];
 
         //Initialize countArr which counts the number of times an Integer
         //appear and index = integer
         for (int i = 0; i < inputArr.length; i++) {
-            countArr[inputArr[i]]++;
+            countArr[inputArr[i] - min]++;
         }
 
         //countArr[i] now stores countArr[i-1] + countArr[i], which contains
@@ -26,7 +27,7 @@ class CountingSort {
         //   counter (so that repeated values will be place at correct index)
         //3) Store the value into outputArr[countArr[inputArr[i]] - 1]
         for (int i = inputArr.length - 1; i >= 0; i--) {
-            int val = inputArr[i];
+            int val = inputArr[i] - min;
             int pos = countArr[val]--;
             outputArr[pos - 1] = val;
         }
